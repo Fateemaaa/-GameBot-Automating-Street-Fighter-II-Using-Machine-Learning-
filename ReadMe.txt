1c8a0bc0eb3d9c1329ab166bc15549375ee519f3
Fateemaaa <116489459+Fateemaaa@users.noreply.github.com>
2024-06-01 22:43:03 +0500

    Uploaded files
    For the emulator, please mail me at fatiima.azfarr@gmail.com

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+******************************************************************************* READ ME FILE *********************************************************************************************************
+First we downloaded our file containing all the work of the project.
+ we linked the "bot.py" file with our game. 
+to run the game,link the bot and model.py file with your code and in terminal, run the command "python controller.py 1" and then wait for the processing of the model. 
+make sure u have the necessary libraries intalled. 
+when our game will reach to an end and will terminate,
+ at that time all the pre-processing will be shown on the terminal after the command "python controller.py 1" executed on the terminal. 
+The file "bot.py" contains all the functions linked with our respective classes. 
---
+++ bot.py
@@ -0,0 +1,389 @@
+from command import Command
+import numpy as np
+from buttons import Buttons
+import model
+class Bot:
+
+    def __init__(self):
+        #< - v + < - v - v + > - > + Y
+        self.fire_code=["<","!<","v+<","!v+!<","v","!v","v+>","!v+!>",">+Y","!>+!Y"]
+        self.exe_code = 0
+        self.start_fire=True
+        self.remaining_code=[]
+        self.my_command = Command()
+        self.buttn= Buttons()
+
+    def fight(self,current_game_state,player):
+
+
+
+        #access class variables of game_state.py
+
+        print("player1 x coord: ",current_game_state.player1.x_coord)
+        print("player2 x coord: ",current_game_state.player2.x_coord)
+        #access buttons from buttons.py
+        print("player1 buttons: ",current_game_state.player1.player_buttons.up)
+        print("player1 buttons: ",current_game_state.player2.player_buttons.down)
+        print("player1 buttons: ",current_game_state.player2.player_buttons.left)
+        print("player1 buttons: ",current_game_state.player2.player_buttons.right)
+  
+
+        print("player2 buttons: ", current_game_state.player2.player_buttons.up)
+        print("player2 buttons: ", current_game_state.player2.player_buttons.down)
+        print("player2 buttons: ", current_game_state.player2.player_buttons.left)
+        print("player2 buttons: ", current_game_state.player2.player_buttons.right)
+        
+
+    
+
+
+        print("player1 health: ",current_game_state.player1.health)
+        print("player2 health: ",current_game_state.player2.health)
+
+        print("time: ",current_game_state.timer)
+
+        print("round started: ",current_game_state.has_round_started)
+        print("round over: ",current_game_state.is_round_over)
+        print("fight result: ",current_game_state.fight_result)
+
+        print("Player1 is jumping: ",current_game_state.player1.is_jumping)
+        print("Player2 is jumping: ",current_game_state.player2.is_jumping)
+
+        print("Player1 is crouching: ",current_game_state.player1.is_crouching)
+        print("Player2 is crouching: ",current_game_state.player2.is_crouching)
+
+        print("Player1 is in move: ",current_game_state.player1.is_player_in_move)
+        print("Player2 is in move: ",current_game_state.player2.is_player_in_move)
+
+        import csv
+
+        # create a list of dictionaries for each row
+      
+        #store rows in df in csv using concat method 
+        import pandas as pd
+
+            # Load existing data from CSV file or create an empty DataFrame
+        try:
+                df = pd.read_csv('file.csv')
+        except FileNotFoundError:
+            df = pd.DataFrame()
+
+        # Create a new DataFrame from variables
+        new_data = pd.DataFrame({
+            'is_round_over': [current_game_state.is_round_over],
+            'fight_result': [current_game_state.fight_result],
+            'player1_health': [current_game_state.player1.health],
+            'player2_health': [current_game_state.player2.health],
+            'player1_x_coord': [current_game_state.player1.x_coord],
+            'player2_x_coord': [current_game_state.player2.x_coord],
+            'player1_y_coord': [current_game_state.player1.y_coord],
+            'player2_y_coord': [current_game_state.player2.y_coord],
+            'player1_is_jumping': [current_game_state.player1.is_jumping],
+            'player2_is_jumping': [current_game_state.player2.is_jumping],
+            'player1_is_crouching': [current_game_state.player1.is_crouching],
+            'player2_is_crouching': [current_game_state.player2.is_crouching],
+            'player1_is_player_in_move': [current_game_state.player1.is_player_in_move],
+            'player2_is_player_in_move': [current_game_state.player2.is_player_in_move],
+            'player1_buttons_up': [current_game_state.player1.player_buttons.up],
+            'player1_buttons_down': [current_game_state.player1.player_buttons.down],
+            'player1_buttons_left': [current_game_state.player1.player_buttons.left],
+            'player1_buttons_right': [current_game_state.player1.player_buttons.right],
+            'player2_buttons_up': [current_game_state.player2.player_buttons.up],
+            'player2_buttons_down': [current_game_state.player2.player_buttons.down],
+            'player2_buttons_left': [current_game_state.player2.player_buttons.left],
+            'player2_buttons_right': [current_game_state.player2.player_buttons.right],
+            'timer': [current_game_state.timer],
+            'has_round_started': [current_game_state.has_round_started],
+        })
+
+        # Concatenate the existing and new data using the concat function
+        df = pd.concat([df, new_data], ignore_index=True)
+
+        # Write the updated DataFrame back to the CSV file
+        df.to_csv('file.csv', index=False)
+
+    
+        #import model file and run it
+        import subprocess
+        subprocess.call(["python", "model.py"])
+
+        #python Videos\gamebot-competition-master\PythonAPI\controller.py 1
+        if player=="1":
+            #print("1")
+            #v - < + v - < + B spinning
+
+            if( self.exe_code!=0  ):
+               self.run_command([],current_game_state.player1)
+            diff=current_game_state.player2.x_coord - current_game_state.player1.x_coord
+            if (  diff > 60 ) :
+                toss=np.random.randint(3)
+                if (toss==0):
+                    #self.run_command([">+^+Y",">+^+Y",">+^+Y","!>+!^+!Y"],current_game_state.player1)
+                    self.run_command([">","-","!>","v+>","-","!v+!>","v","-","!v","v+<","-","!v+!<","<+Y","-","!<+!Y"],current_game_state.player1)
+                elif ( toss==1 ):
+                    self.run_command([">+^+B",">+^+B","!>+!^+!B"],current_game_state.player1)
+                else: #fire
+                    self.run_command(["<","-","!<","v+<","-","!v+!<","v","-","!v","v+>","-","!v+!>",">+Y","-","!>+!Y"],current_game_state.player1)
+            elif (  diff < -60 ) :
+                toss=np.random.randint(3)
+                if (toss==0):#spinning
+                    #self.run_command(["<+^+Y","<+^+Y","<+^+Y","!<+!^+!Y"],current_game_state.player1)
+                    self.run_command(["<","-","!<","v+<","-","!v+!<","v","-","!v","v+>","-","!v+!>",">+Y","-","!>+!Y"],current_game_state.player1)
+                elif ( toss==1):#
+                    self.run_command(["<+^+B","<+^+B","!<+!^+!B"],current_game_state.player1)
+                else: #fire
+                    self.run_command([">","-","!>","v+>","-","!v+!>","v","-","!v","v+<","-","!v+!<","<+Y","-","!<+!Y"],current_game_state.player1)
+            else:
+                toss=np.random.randint(2)  # anyFightActionIsTrue(current_game_state.player2.player_buttons)
+                if ( toss>=1 ):
+                    if (diff>0):
+                        self.run_command(["<","<","!<"],current_game_state.player1)
+                    else:
+                        self.run_command([">",">","!>"],current_game_state.player1)
+                else:
+                    self.run_command(["v+R","v+R","v+R","!v+!R"],current_game_state.player1)
+            self.my_command.player_buttons=self.buttn
+
+        elif player=="2":
+
+            if( self.exe_code!=0  ):
+               self.run_command([],current_game_state.player2)
+            diff=current_game_state.player1.x_coord - current_game_state.player2.x_coord
+            if (  diff > 60 ) :
+                toss=np.random.randint(3)
+                if (toss==0):
+                    #self.run_command([">+^+Y",">+^+Y",">+^+Y","!>+!^+!Y"],current_game_state.player2)
+                    self.run_command([">","-","!>","v+>","-","!v+!>","v","-","!v","v+<","-","!v+!<","<+Y","-","!<+!Y"],current_game_state.player2)
+                elif ( toss==1 ):
+                    self.run_command([">+^+B",">+^+B","!>+!^+!B"],current_game_state.player2)
+                else:
+                    self.run_command(["<","-","!<","v+<","-","!v+!<","v","-","!v","v+>","-","!v+!>",">+Y","-","!>+!Y"],current_game_state.player2)
+            elif ( diff < -60 ) :
+                toss=np.random.randint(3)
+                if (toss==0):
+                    #self.run_command(["<+^+Y","<+^+Y","<+^+Y","!<+!^+!Y"],current_game_state.player2)
+                    self.run_command(["<","-","!<","v+<","-","!v+!<","v","-","!v","v+>","-","!v+!>",">+Y","-","!>+!Y"],current_game_state.player2)
+                elif ( toss==1):
+                    self.run_command(["<+^+B","<+^+B","!<+!^+!B"],current_game_state.player2)
+                else:
+                    self.run_command([">","-","!>","v+>","-","!v+!>","v","-","!v","v+<","-","!v+!<","<+Y","-","!<+!Y"],current_game_state.player2)
+            else:
+                toss=np.random.randint(2)  # anyFightActionIsTrue(current_game_state.player2.player_buttons)
+                if ( toss>=1 ):
+                    if (diff<0):
+                        self.run_command(["<","<","!<"],current_game_state.player2)
+                    else:
+                        self.run_command([">",">","!>"],current_game_state.player2)
+                else:
+                    self.run_command(["v+R","v+R","v+R","!v+!R"],current_game_state.player2)
+            self.my_command.player2_buttons=self.buttn
+        return self.my_command
+
+
+
+    def run_command( self , com , player   ):
+
+        if self.exe_code-1==len(self.fire_code):
+            self.exe_code=0
+            self.start_fire=False
+            print ("compelete")
+            #exit()
+            # print ( "left:",player.player_buttons.left )
+            # print ( "right:",player.player_buttons.right )
+            # print ( "up:",player.player_buttons.up )
+            # print ( "down:",player.player_buttons.down )
+            # print ( "Y:",player.player_buttons.Y )
+
+        elif len(self.remaining_code)==0 :
+
+            self.fire_code=com
+            #self.my_command=Command()
+            self.exe_code+=1
+
+            self.remaining_code=self.fire_code[0:]
+
+        else:
+            self.exe_code+=1
+            if self.remaining_code[0]=="v+<":
+                self.buttn.down=True
+                self.buttn.left=True
+                print("v+<")
+            elif self.remaining_code[0]=="!v+!<":
+                self.buttn.down=False
+                self.buttn.left=False
+                print("!v+!<")
+            elif self.remaining_code[0]=="v+>":
+                self.buttn.down=True
+                self.buttn.right=True
+                print("v+>")
+            elif self.remaining_code[0]=="!v+!>":
+                self.buttn.down=False
+                self.buttn.right=False
+                print("!v+!>")
+
+            elif self.remaining_code[0]==">+Y":
+                self.buttn.Y= True #not (player.player_buttons.Y)
+                self.buttn.right=True
+                print(">+Y")
+            elif self.remaining_code[0]=="!>+!Y":
+                self.buttn.Y= False #not (player.player_buttons.Y)
+                self.buttn.right=False
+                print("!>+!Y")
+
+            elif self.remaining_code[0]=="<+Y":
+                self.buttn.Y= True #not (player.player_buttons.Y)
+                self.buttn.left=True
+                print("<+Y")
+            elif self.remaining_code[0]=="!<+!Y":
+                self.buttn.Y= False #not (player.player_buttons.Y)
+                self.buttn.left=False
+                print("!<+!Y")
+
+            elif self.remaining_code[0]== ">+^+L" :
+                self.buttn.right=True
+                self.buttn.up=True
+                self.buttn.L= not (player.player_buttons.L)
+                print(">+^+L")
+            elif self.remaining_code[0]== "!>+!^+!L" :
+                self.buttn.right=False
+                self.buttn.up=False
+                self.buttn.L= False #not (player.player_buttons.L)
+                print("!>+!^+!L")
+
+            elif self.remaining_code[0]== ">+^+Y" :
+                self.buttn.right=True
+                self.buttn.up=True
+                self.buttn.Y= not (player.player_buttons.Y)
+                print(">+^+Y")
+            elif self.remaining_code[0]== "!>+!^+!Y" :
+                self.buttn.right=False
+                self.buttn.up=False
+                self.buttn.Y= False #not (player.player_buttons.L)
+                print("!>+!^+!Y")
+
+
+            elif self.remaining_code[0]== ">+^+R" :
+                self.buttn.right=True
+                self.buttn.up=True
+                self.buttn.R= not (player.player_buttons.R)
+                print(">+^+R")
+            elif self.remaining_code[0]== "!>+!^+!R" :
+                self.buttn.right=False
+                self.buttn.up=False
+                self.buttn.R= False #ot (player.player_buttons.R)
+                print("!>+!^+!R")
+
+            elif self.remaining_code[0]== ">+^+A" :
+                self.buttn.right=True
+                self.buttn.up=True
+                self.buttn.A= not (player.player_buttons.A)
+                print(">+^+A")
+            elif self.remaining_code[0]== "!>+!^+!A" :
+                self.buttn.right=False
+                self.buttn.up=False
+                self.buttn.A= False #not (player.player_buttons.A)
+                print("!>+!^+!A")
+
+            elif self.remaining_code[0]== ">+^+B" :
+                self.buttn.right=True
+                self.buttn.up=True
+                self.buttn.B= not (player.player_buttons.B)
+                print(">+^+B")
+            elif self.remaining_code[0]== "!>+!^+!B" :
+                self.buttn.right=False
+                self.buttn.up=False
+                self.buttn.B= False #not (player.player_buttons.A)
+                print("!>+!^+!B")
+
+            elif self.remaining_code[0]== "<+^+L" :
+                self.buttn.left=True
+                self.buttn.up=True
+                self.buttn.L= not (player.player_buttons.L)
+                print("<+^+L")
+            elif self.remaining_code[0]== "!<+!^+!L" :
+                self.buttn.left=False
+                self.buttn.up=False
+                self.buttn.L= False  #not (player.player_buttons.Y)
+                print("!<+!^+!L")
+
+            elif self.remaining_code[0]== "<+^+Y" :
+                self.buttn.left=True
+                self.buttn.up=True
+                self.buttn.Y= not (player.player_buttons.Y)
+                print("<+^+Y")
+            elif self.remaining_code[0]== "!<+!^+!Y" :
+                self.buttn.left=False
+                self.buttn.up=False
+                self.buttn.Y= False  #not (player.player_buttons.Y)
+                print("!<+!^+!Y")
+
+            elif self.remaining_code[0]== "<+^+R" :
+                self.buttn.left=True
+                self.buttn.up=True
+                self.buttn.R= not (player.player_buttons.R)
+                print("<+^+R")
+            elif self.remaining_code[0]== "!<+!^+!R" :
+                self.buttn.left=False
+                self.buttn.up=False
+                self.buttn.R= False  #not (player.player_buttons.Y)
+                print("!<+!^+!R")
+
+            elif self.remaining_code[0]== "<+^+A" :
+                self.buttn.left=True
+                self.buttn.up=True
+                self.buttn.A= not (player.player_buttons.A)
+                print("<+^+A")
+            elif self.remaining_code[0]== "!<+!^+!A" :
+                self.buttn.left=False
+                self.buttn.up=False
+                self.buttn.A= False  #not (player.player_buttons.Y)
+                print("!<+!^+!A")
+
+            elif self.remaining_code[0]== "<+^+B" :
+                self.buttn.left=True
+                self.buttn.up=True
+                self.buttn.B= not (player.player_buttons.B)
+                print("<+^+B")
+            elif self.remaining_code[0]== "!<+!^+!B" :
+                self.buttn.left=False
+                self.buttn.up=False
+                self.buttn.B= False  #not (player.player_buttons.Y)
+                print("!<+!^+!B")
+
+            elif self.remaining_code[0]== "v+R" :
+                self.buttn.down=True
+                self.buttn.R= not (player.player_buttons.R)
+                print("v+R")
+            elif self.remaining_code[0]== "!v+!R" :
+                self.buttn.down=False
+                self.buttn.R= False  #not (player.player_buttons.Y)
+                print("!v+!R")
+
+            else:
+                if self.remaining_code[0] =="v" :
+                    self.buttn.down=True
+                    print ( "down" )
+                elif self.remaining_code[0] =="!v":
+                    self.buttn.down=False
+                    print ( "Not down" )
+                elif self.remaining_code[0] =="<" :
+                    print ( "left" )
+                    self.buttn.left=True
+                elif self.remaining_code[0] =="!<" :
+                    print ( "Not left" )
+                    self.buttn.left=False
+                elif self.remaining_code[0] ==">" :
+                    print ( "right" )
+                    self.buttn.right=True
+                elif self.remaining_code[0] =="!>" :
+                    print ( "Not right" )
+                    self.buttn.right=False
+
+                elif self.remaining_code[0] =="^" :
+                    print ( "up" )
+                    self.buttn.up=True
+                elif self.remaining_code[0] =="!^" :
+                    print ( "Not up" )
+                    self.buttn.up=False
+            self.remaining_code=self.remaining_code[1:]
+        return
